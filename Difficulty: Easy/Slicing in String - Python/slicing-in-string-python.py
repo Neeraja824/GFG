7fa1c11f037a6ab590a7f72ece08d513e06b762e
Neeraja824 <155799322+Neeraja824@users.noreply.github.com>
2025-10-16 10:56:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Slicing in String - Python/slicing-in-string-python.py
@@ -0,0 +1,7 @@
+# Function to join given bound_by and tag
+def join_middle(bound_by, tag_name):
+  # complete the statement below to return the string as required
+  mid=len(bound_by)//2
+  return bound_by[0 :  mid  ] + tag_name + bound_by[mid:  ]
+  
+ 

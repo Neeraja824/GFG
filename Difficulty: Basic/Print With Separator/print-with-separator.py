681e19b6cad4fc3d273d661b2e2c6cadcb44ba9e
Neeraja824 <155799322+Neeraja824@users.noreply.github.com>
2025-12-14 20:16:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Separator/print-with-separator.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+
+def utility():
+    a = input()
+    b = input()
+    separator = input()
+    print(a,end=separator)
+    print(b)
+    #Your code below. Use sep=

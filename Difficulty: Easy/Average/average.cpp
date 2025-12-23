fb58177421c7f033a7b66aeba52f5bc3b2388cd8
Neeraja824 <155799322+Neeraja824@users.noreply.github.com>
2025-12-23 21:35:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Average/average.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+int posAverage(int arr[], int size) {
+
+    // Write your code to find average of positive numbers in arr array
+    // return the answer
+    int count=0;
+        int avg=0;
+        for(int i=0;i<size;i++){
+            if(arr[i]>=0){
+                avg+=arr[i];
+                count++;
+            }
+        }
+        return avg/count;
+}
